08676abca526e36e2d560de60a893ed0a4ea0484
Daniel Jung <daniel.j11@gso.schule.koeln>
2022-09-08 12:15:32 +0200

    update

--- GrundlagenCsharp/Program.cs
+++ GrundlagenCsharp/Program.cs
@@ -1,6 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello");
-
+﻿Console.WriteLine(" .-.");
+Console.WriteLine("(o o)");
+Console.WriteLine("| O |");
+Console.WriteLine("|   |");
+Console.WriteLine("'~~~'");
 
 
 //test
